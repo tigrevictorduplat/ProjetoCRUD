e319d25a81472f59f3a1ff16a25a405b88661ad1
Víctor Duplat Tigre <98099656+tigrevictorduplat@users.noreply.github.com>
2024-09-05 19:00:53 -0300

    Revert "Versão Sam - Ajustes"
    This reverts commit 483cad235148d4874733f0a5643dc8dd6de0fc0f.

--- ClassesPrincipais/Pessoa.java
+++ ClassesPrincipais/Pessoa.java
@@ -4,25 +4,19 @@ public class Pessoa {
     private String CPF;
     private String nomePessoa;
     private String enderecoPessoa;
-    private int idFuncao;
     
-    
-
     //Construtores
+    public Pessoa(String nomeDono, String enderecoDono) {
+        this.nomePessoa = nomeDono;
+        this.enderecoPessoa = enderecoDono;
+    }
+
     public Pessoa(String CPF, String nomeDono, String enderecoDono) {
         this.CPF = CPF;
         this.nomePessoa = nomeDono;
         this.enderecoPessoa = enderecoDono;
     }
 
-    // Getters e Setters
-public int getIdFuncao() {
-        return idFuncao;
-    }
-
-    public void setIdFuncao(int idFuncao) {
-        this.idFuncao = idFuncao;
-    }
     public String getCPF() {
         return CPF;
     }
